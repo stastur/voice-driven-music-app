1d2b8efa69a3692712c216399ccc5e3fca868417
Stas Tur <stastyr123@gmail.com>
2020-04-04 12:29:16 +0300

    Updates

---
+++ precache-manifest.d20085496fa3cbcb95fc9e0d17c557b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bebc471ad9d029de6c6af3bb3fedb4e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7bc4e7488fb0e9e60c93",
+    "url": "./static/css/main.e0f7d66d.chunk.css"
+  },
+  {
+    "revision": "8db909099bd2e8b4bda8",
+    "url": "./static/js/2.215fdca2.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "./static/js/2.215fdca2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bc4e7488fb0e9e60c93",
+    "url": "./static/js/main.9893c87c.chunk.js"
+  },
+  {
+    "revision": "e6c4f24bf6999049996a",
+    "url": "./static/js/runtime-main.81cefd07.js"
+  }
+]);
